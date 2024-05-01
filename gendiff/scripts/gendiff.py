8646c466ca2c7662db1f51fd21f8945e22aafa42
Konstantin Sereda <konst.sereda@gmail.com>
2024-05-01 21:00:27 +0500

    linter E501 line too long

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,12 +5,14 @@ import json
 
 parser = argparse.ArgumentParser(add_help=False,
                                  prog='gendiff',
-                                 description='Compares two configuration files and shows a difference.',
+                                 description='Compares two configuration '
+                                             'files and shows a difference.',
                                  epilog='epilog')
 parser.add_argument('first_file', type=str, help='')
 parser.add_argument('second_file', type=str, help='')
 parser.add_argument('-v', '--version', action='version',
-                    version='%(prog)s 1.0', help="Show program's version number and exit.")
+                    version='%(prog)s 1.0', help="Show program's version "
+                                                 "number and exit.")
 parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
                     help='Show this help message and exit.')
 parser.add_argument('-f', '--format', default=argparse.SUPPRESS,
