aa8dcfc8c6d78b9d4ce5a3afa8b90aba1e8f4a06
Konstantin Sereda <konst.sereda@gmail.com>
2024-05-10 09:58:06 +0500

    add test for flat yaml files

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -17,6 +17,16 @@ def test_flat_json():
     assert filecmp.cmp(result_testing_func, correct_output)
 
 
+def test_flat_yaml():
+    file_output = open('tests/fixtures/output.txt', 'w')
+    file_output.write(generate_diff('tests/fixtures/file1.yaml',
+                                    'tests/fixtures/file2.yaml'))
+    file_output.close()
+    result_testing_func = 'tests/fixtures/output.txt'
+    correct_output = 'tests/fixtures/correct_output'
+    assert filecmp.cmp(result_testing_func, correct_output)
+
+
 def test_cli():
     paths = parse(['tests/fixtures/file1.json', 'tests/fixtures/file2.json'])
     assert paths.first_file == 'tests/fixtures/file1.json'
