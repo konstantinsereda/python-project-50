ce1310c11c2c22fc2c04aaca1bea979bd782431f
Konstantin Sereda <konst.sereda@gmail.com>
2024-05-11 11:57:21 +0500

    modify output generate_diff() and tests

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,10 +1,9 @@
+# import json
 from gendiff.cli import parse
 from gendiff.file_parcer import get_data
 
 
-def generate_diff(file_path1, file_path2):
-    data1 = get_data(file_path1)
-    data2 = get_data(file_path2)
+def generate_diff(data1, data2):
 
     keys = sorted(set(data1.keys()) | set(data2.keys()))
 
@@ -15,17 +14,28 @@ def generate_diff(file_path1, file_path2):
         elif key not in data2:
             diff.append(f'- {key}: {data1[key]}')
         elif data1[key] != data2[key]:
-            diff.append(f'- {key}: {data1[key]}')
-            diff.append(f'+ {key}: {data2[key]}')
+            if not isinstance(data1[key], dict) and not isinstance(data2[key], dict):
+                diff.append(f'- {key}: {data1[key]}')
+                diff.append(f'+ {key}: {data2[key]}')
+            else:
+                pass
         elif data1[key] == data2[key]:
             diff.append(f'  {key}: {data1[key]}')
 
-    return '{\n  ' + '\n  '.join(diff) + '\n}'
+#    return '{\n  ' + '\n  '.join(diff) + '\n}'
+    return diff
 
 
 def main():
     args = parse()
-    print(generate_diff(args.first_file, args.second_file))
+    data1 = get_data(args.first_file)
+    data2 = get_data(args.second_file)
+    diff = generate_diff(data1, data2)
+    print(type(diff))
+    print(diff)
+    result = '{\n  ' + '\n  '.join(diff) + '\n}'
+    print(type(result))
+    print(result)
 
 
 if __name__ == '__main__':
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,3 +1,5 @@
+import json
+import yaml
 import pytest
 import sys
 import filecmp
@@ -5,21 +7,31 @@ from gendiff.cli import parse
 from gendiff.scripts.gendiff import main
 from gendiff.scripts.gendiff import generate_diff
 from unittest.mock import patch
+from gendiff.file_parcer import get_data
+
+
+def test_get_data():
+    pass
 
 
 def test_flat_json():
+    data1 = get_data('tests/fixtures/file1.json')
+    data2 = get_data('tests/fixtures/file2.json')
     file_output = open('tests/fixtures/output.txt', 'w')
-    file_output.write(generate_diff('tests/fixtures/file1.json',
-                                    'tests/fixtures/file2.json'))
+    diff = generate_diff(data1, data2)
+    file_output.write('{\n  ' + '\n  '.join(diff) + '\n}')
     file_output.close()
     result_testing_func = 'tests/fixtures/output.txt'
     correct_output = 'tests/fixtures/correct_output'
     assert filecmp.cmp(result_testing_func, correct_output)
 
+
 def test_cascaded_json():
+    data1 = get_data('tests/fixtures/file_cascaded1.json')
+    data2 = get_data('tests/fixtures/file_cascaded2.json')
     file_output = open('tests/fixtures/output.txt', 'w')
-    file_output.write(generate_diff('tests/fixtures/file_cascaded1.json',
-                                    'tests/fixtures/file_cascaded2.json'))
+    diff = generate_diff(data1, data2)
+    file_output.write(json.dumps(diff, indent=4))
     file_output.close()
     result_testing_func = 'tests/fixtures/output.txt'
     correct_output = 'tests/fixtures/correct_output_cascaded'
@@ -27,9 +39,11 @@ def test_cascaded_json():
 
 
 def test_flat_yaml():
+    data1 = get_data('tests/fixtures/file1.yaml')
+    data2 = get_data('tests/fixtures/file2.yaml')
     file_output = open('tests/fixtures/output.txt', 'w')
-    file_output.write(generate_diff('tests/fixtures/file1.yaml',
-                                    'tests/fixtures/file2.yaml'))
+    diff = generate_diff(data1, data2)
+    file_output.write('{\n  ' + '\n  '.join(diff) + '\n}')
     file_output.close()
     result_testing_func = 'tests/fixtures/output.txt'
     correct_output = 'tests/fixtures/correct_output'
